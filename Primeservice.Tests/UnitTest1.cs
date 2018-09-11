e210aea7d9d1698566633573e90370cae0990a44
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-11 10:59:04 +0200

    Tilføjet testmappe unittest

---
+++ Primeservice.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Primeservice.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
